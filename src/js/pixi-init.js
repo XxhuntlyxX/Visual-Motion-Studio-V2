2e8f2e0a906b400d3b6baf6a84a61745d4d57d17
XxhuntlyxX <jason.huntly.grobler@gmail.com>
2025-07-03 22:52:19 +0200

    Update pixi-init.js

--- src/js/pixi-init.js
+++ src/js/pixi-init.js
@@ -1,17 +1,14 @@
-import * as PIXI from 'https://cdn.skypack.dev/pixi.js';
+import * as PIXI from "https://unpkg.com/pixi.js@8.0.0/dist/pixi.mjs";
 
 export let pixiApp = null;
 
 export async function initPixi() {
   const container = document.getElementById('pixi-canvas-container');
   container.innerHTML = "";
-
-  // PixiJS v8+ requires async initialization
   pixiApp = await PIXI.Application.init({
     width: 640,
     height: 360,
     background: 0x222222,
   });
-
-  container.appendChild(pixiApp.canvas); // Was .view, now .canvas in v8+
+  container.appendChild(pixiApp.canvas);
 }
